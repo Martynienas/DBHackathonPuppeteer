1322f2bbb188e07328f54e53d08581b4fdfb5338
Martynas Luneckas <m.luneckas@live.com>
2019-05-09 21:20:43 +0300

    Formatting

--- DBHackathonPuppeteer/Program.cs
+++ DBHackathonPuppeteer/Program.cs
@@ -6,34 +6,35 @@ using PuppeteerSharp;
 
 namespace PuppeteerSharpPdfDemo
 {
-    class MainClass
-    {
-        public static async Task Main(string[] args)
-        {
-            var options = new LaunchOptions
-            {
-                Headless = true
-            };
-            Console.WriteLine("Downloading chromium");
-            await new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultRevision);
-            MyLogger.SetConfiguration();
-            MyLogger.Info("Navigating to puppeteersharp");
-            using (var browser = await Puppeteer.LaunchAsync(options))
-            using (var page = await browser.NewPageAsync())
-            {
-                await page.GoToAsync("https://www.puppeteersharp.com/examples/index.html");
-                MyLogger.Info("Generating PDF!");
-                await page.PdfAsync(Path.Combine(Directory.GetCurrentDirectory(), "puppeteersharp.pdf"));
-                MyLogger.Info("Export completed");
-                await page.GoToAsync("https://www.aymen-loukil.com/en/blog-en/google-puppeteer-tutorial-with-examples/");
-                MyLogger.Info("Generating PDF");
-                await page.PdfAsync(Path.Combine(Directory.GetCurrentDirectory(), "puppeteer-tutorial.pdf"));
-                MyLogger.Info("Export completed");
-                if (!args.Any(arg => arg == "auto-exit"))
-                {
-                    Console.ReadLine();
-                }
-            }
-        }
-    }
+	class MainClass
+	{
+		public static async Task Main(string[] args)
+		{
+			var options = new LaunchOptions
+			{
+				 Headless = true
+			};
+			Logger.SetConfiguration();
+			Logger.Info("Downloading chromium");
+			await new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultRevision);
+			using (var browser = await Puppeteer.LaunchAsync(options))
+			using (var page = await browser.NewPageAsync())
+			{
+				Logger.Info("Navigating to puppeteersharp");
+				await page.GoToAsync("https://www.puppeteersharp.com/examples/index.html");
+				Logger.Info("Generating PDF!");
+				await page.PdfAsync(Path.Combine(Directory.GetCurrentDirectory(), "puppeteersharp.pdf"));
+				Logger.Info("Export completed");
+				Logger.Info("Navigating to aymen-loukil");
+				await page.GoToAsync("https://www.aymen-loukil.com/en/blog-en/google-puppeteer-tutorial-with-examples/");
+				Logger.Info("Generating PDF");
+				await page.PdfAsync(Path.Combine(Directory.GetCurrentDirectory(), "puppeteer-tutorial.pdf"));
+				Logger.Info("Export completed");
+				if (!args.Any(arg => arg == "auto-exit"))
+				{
+					Console.ReadLine();
+				}
+			}
+		}
+	}
 }
--- DBHackathonPuppeteer/log4net.cs
+++ DBHackathonPuppeteer/log4net.cs
@@ -2,48 +2,48 @@
 
 namespace PuppeteerSharpPdfDemo
 {
-    static class MyLogger
-    {
-        static ILog logger = null;
-
-        public static void SetConfiguration()
-        {
-            logger = LogManager.GetLogger(typeof(MyLogger));
-            var l = (log4net.Repository.Hierarchy.Logger)logger.Logger;
-            l.Level = log4net.Core.Level.Trace;
-            AddConsoleAppender();
-        }
-
-        public static void Debug(string message)
-        {
-            logger.Debug(message);
-        }
-
-        public static void Info(string message)
-        {
-            logger.Info(message);
-        }
-
-        // Others here
-
-        static private void AddConsoleAppender()
-        {
-            var appender = new log4net.Appender.AnsiColorTerminalAppender
-            {
-                Threshold = log4net.Core.Level.Info
-            };
-            var l = (log4net.Repository.Hierarchy.Logger)logger.Logger;
-            log4net.Layout.PatternLayout layout = new log4net.Layout.PatternLayout
-            {
-                ConversionPattern = "%d [%t] %-5p %c [%x] - %m%n"
-            };
-            layout.ActivateOptions();
-            appender.Name = "Console";
-            appender.Layout = layout;
-            appender.ActivateOptions();
-            l.AddAppender(appender);
-            l.Repository.Configured = true;
-        }
-    }
-
+	static class Logger
+	{
+		static ILog logger = null;
+		
+		public static void SetConfiguration()
+		{
+			logger = LogManager.GetLogger(typeof(Logger));
+			var l = (log4net.Repository.Hierarchy.Logger)logger.Logger;
+			l.Level = log4net.Core.Level.Trace;
+			AddConsoleAppender();
+		}
+		
+		public static void Debug(string message)
+		{
+			logger.Debug(message);
+		}
+		
+		public static void Info(string message)
+		{
+			logger.Info(message);
+		}
+		
+		// Others here
+		
+		static private void AddConsoleAppender()
+		{
+			var appender = new log4net.Appender.AnsiColorTerminalAppender
+			{
+				Threshold = log4net.Core.Level.Info
+			};
+			var l = (log4net.Repository.Hierarchy.Logger)logger.Logger;
+			log4net.Layout.PatternLayout layout = new log4net.Layout.PatternLayout
+			{
+				ConversionPattern = "%d [%t] %-5p %c [%x] - %m%n"
+			};
+			layout.ActivateOptions();
+			appender.Name = "Console";
+			appender.Layout = layout;
+			appender.ActivateOptions();
+			l.AddAppender(appender);
+			l.Repository.Configured = true;
+		}
+	}
+	
 }
