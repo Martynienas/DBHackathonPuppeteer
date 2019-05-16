f1f6012d28ff99e79528cca286b27b035822f69e
Martynas Luneckas <m.luneckas@live.com>
2019-05-16 12:59:02 +0300

    Initial working solver

--- DBHackathonPuppeteer/GameSolverHelper.cs
+++ DBHackathonPuppeteer/GameSolverHelper.cs
@@ -1,6 +1,7 @@
 ﻿using PuppeteerSharp;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,22 +9,22 @@ namespace DBHackathonPuppeteer
 {
     class GameSolverHelper
     {
-        public static int[] gridTiles = 
-           {
-            0, 0, 0, 0,
-            0, 0, 0, 0,
-            0, 0, 0, 0,
-            0, 0, 0, 0
-           };
-
         public class Tile
         {
             public int Index { get; set; }
             public int Value { get; set; }
         }
 
-        public async  void GetGridElements(Page page)
+        public async Task<int[]> GetGridElements(Page page)
         {
+            int[] gridTiles =
+           {
+                0, 0, 0, 0,
+            0, 0, 0, 0,
+            0, 0, 0, 0,
+            0, 0, 0, 0
+           }; ;
+
             var jsCode = @"() => {
                         const selectors = Array.from(document.querySelectorAll('div.tile-container > div.tile')); 
                         return selectors.map( t=> {return { Index: t.getAttribute('data-index'), Value: t.getAttribute('data-val') }});
@@ -34,7 +35,29 @@ namespace DBHackathonPuppeteer
                 gridTiles[result.Index] = result.Value;
             }
 
-            //var cleanUrlList = test.Result.ToArray();
+            return gridTiles;
+
+        }
+
+        public async Task Jiggle(Page page)
+        {
+            
+            for (int i = 1; i < 10000; i++)
+            {
+                int[] tempGrid = await GetGridElements(page);
+                await page.Keyboard.PressAsync("ArrowUp");
+                await page.Keyboard.PressAsync("ArrowRight");
+                int[] tempGrid2 = await GetGridElements(page);
+                bool isEqual = Enumerable.SequenceEqual(tempGrid, tempGrid2);
+                if (isEqual)
+                {
+                    await page.Keyboard.PressAsync("ArrowDown");
+                                        if (Enumerable.SequenceEqual(tempGrid, await GetGridElements(page)))
+                    {
+                        await page.Keyboard.PressAsync("ArrowLeft");
+                    }
+                }
+            }
         }
 
     }
