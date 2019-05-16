c3814b34b5ea0738e70cdb437f53301bbf417b3a
Martynas Luneckas <m.luneckas@live.com>
2019-05-16 10:21:14 +0300

    Clean-up

--- DBHackathonPuppeteer/Program.cs
+++ DBHackathonPuppeteer/Program.cs
@@ -21,15 +21,7 @@ namespace PuppeteerSharpPdfDemo
 			using (var page = await browser.NewPageAsync())
 			{
 				Logger.Info("Navigating to puppeteersharp");
-				await page.GoToAsync("https://www.puppeteersharp.com/examples/index.html");
-				Logger.Info("Generating PDF!");
-				await page.PdfAsync(Path.Combine(Directory.GetCurrentDirectory(), "puppeteersharp.pdf"));
-				Logger.Info("Export completed");
-				Logger.Info("Navigating to aymen-loukil");
-				await page.GoToAsync("https://www.aymen-loukil.com/en/blog-en/google-puppeteer-tutorial-with-examples/");
-				Logger.Info("Generating PDF");
-				await page.PdfAsync(Path.Combine(Directory.GetCurrentDirectory(), "puppeteer-tutorial.pdf"));
-				Logger.Info("Export completed");
+				await page.GoToAsync("https://4ark.me/2048/");
 				if (!args.Any(arg => arg == "auto-exit"))
 				{
 					Console.ReadLine();
