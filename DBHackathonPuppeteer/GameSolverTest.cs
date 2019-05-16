455712c0a9f4b71e4d104e9d9d6a62e6b8ff8970
Martynas Luneckas <m.luneckas@live.com>
2019-05-16 11:40:08 +0300

    Added Game solver classes

---
+++ DBHackathonPuppeteer/GameSolverTest.cs
@@ -0,0 +1,62 @@
+﻿using NUnit.Framework;
+using PuppeteerSharp;
+using System;
+using System.Threading.Tasks;
+
+namespace DBHackathonPuppeteer
+{
+    [TestFixture]
+    class GameSolverTest
+    {
+        private Browser browser;
+        private Page page;
+ 
+        
+        private const string testPageUrl = "http://4ark.me/2048/";
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            InitializePage().Wait();
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            page.Dispose();
+            browser.Dispose();
+        }
+
+        [Test]
+        public async Task AssertNavigationToUrl()
+        {
+            await page.GoToAsync(testPageUrl);
+            Assert.AreEqual(testPageUrl, page.Url);
+        }
+
+        private async Task InitializePage()
+        {
+            string[] browserArgs = { "--start-maximized" };
+
+            var options = new LaunchOptions
+            {
+                Headless = false,
+                Args = browserArgs
+            };
+
+            var viewPortOptions = new ViewPortOptions
+            {
+                Width = 1920,
+                Height = 1080
+            };
+
+            await new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultRevision);
+            var browser = await Puppeteer.LaunchAsync(options);
+            page = await browser.NewPageAsync();
+            await page.SetViewportAsync(viewPortOptions);
+            await page.GoToAsync(testPageUrl);
+            GameSolverHelper solver = new GameSolverHelper();
+            solver.GetGridElements(page);
+        }
+    }
+}
